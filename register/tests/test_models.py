8eac2911af5241fad4a155fdfa85933a4f9fdfc6
maxpipoka <martincardozo@gmail.com>
2024-01-03 00:05:51 -0300

    Test implementing

---
+++ register/tests/test_models.py
@@ -0,0 +1,21 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from ..models import Taxpayer
+
+class TaxpayerTests(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('taxpayer-list')
+        self.data = {
+            'code': 12, 
+            'name': 'Mart', 
+            'dni': 29285854, 
+            'address':'La casa de Martin',
+            'city_address':'El pueblo donde vive martin'}
+        self.taxpayer = Taxpayer.objects.create(**self.data)
+
+    
+    def test_taxpayer_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_OK_200)
--- register/urls.py
+++ register/urls.py
@@ -1,33 +1,30 @@
 from django.urls import include, path
 from rest_framework import routers
-from register.views import (taxpayer,
-                            deceased,
-                            state, county,
-                            burialPermit,
-                            city,
-                            grave,
-                            parcel,
-                            payment,
-                            periodicity,
-                            registrationOffice,
-                            sector,
-                            tax)
+from register.views import (burialPermitViews,
+                            cityViews, countyViews, deceasedViews,
+                            graveViews,
+                            parcelViews,
+                            paymentViews,
+                            periodicityViews,
+                            registrationOfficeViews,
+                            sectorViews, stateViews,
+                            taxViews, taxpayerViews)
 from register import views
 
 router = routers.DefaultRouter()
-router.register(r'taxpayer', taxpayer.TaxpayerViewset, 'taxpayer')
-router.register(r'deceased', deceased.DeceasedViewset, 'deceased')
-router.register(r'state', state.StateViewset, 'state')
-router.register(r'county', county.CountyViewset, 'county')
-router.register(r'city', city.CityViewset, 'city')
-router.register(r'registrationoffice', registrationOffice.RegistrationOfficeViewset, 'registrationoffice')
-router.register(r'burialpermit', burialPermit.BurialPermitViewset, 'burialpermit')
-router.register(r'sector', sector.SectorViewset, 'sector')
-router.register(r'periodicity', periodicity.PeriodicityViewset, 'periodicity')
-router.register(r'parcel', parcel.ParcelViewset, 'parcel')
-router.register(r'grave', grave.GraveViewset, 'grave')
-router.register(r'tax', tax.TaxViewset, 'tax')
-router.register(r'payment', payment.PaymentViewset, 'payment')
+router.register(r'taxpayer', taxpayerViews.TaxpayerViewset, 'taxpayer')
+router.register(r'deceased', deceasedViews.DeceasedViewset, 'deceased')
+router.register(r'state', stateViews.StateViewset, 'state')
+router.register(r'county', countyViews.CountyViewset, 'county')
+router.register(r'city', cityViews.CityViewset, 'city')
+router.register(r'registrationoffice', registrationOfficeViews.RegistrationOfficeViewset, 'registrationoffice')
+router.register(r'burialpermit', burialPermitViews.BurialPermitViewset, 'burialpermit')
+router.register(r'sector', sectorViews.SectorViewset, 'sector')
+router.register(r'periodicity', periodicityViews.PeriodicityViewset, 'periodicity')
+router.register(r'parcel', parcelViews.ParcelViewset, 'parcel')
+router.register(r'grave', graveViews.GraveViewset, 'grave')
+router.register(r'tax', taxViews.TaxViewset, 'tax')
+router.register(r'payment', paymentViews.PaymentViewset, 'payment')
 
 
 urlpatterns = [
